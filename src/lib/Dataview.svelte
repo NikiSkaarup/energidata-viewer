32e167b04f7c9d5540fbf203e207a7e6dcbd8c18
Niki Wix Skaarup <NikiSkaarup@users.noreply.github.com>
2022-09-14 18:50:59 +0200

    fixed modal

--- src/lib/Dataview.svelte
+++ src/lib/Dataview.svelte
@@ -15,17 +15,29 @@
 		if (value === null) return;
 		const url = `https://api.energidataservice.dk/Dataset/${value.datasetName}?timezone=UTC`;
 
-		fetch('https://api.energidataservice.dk/meta/Dataset')
+		fetch(url)
 			.then((resp) => resp.json())
-			.then((json) => (records = json as dataset[]))
-			.catch((e) => {
-				console.log(e);
-			});
+			.then((json) => (records = json as record[]))
+			.catch((e) => console.log(e));
+
+		// disable scrolling
+		document.documentElement.style.top = -document.documentElement.scrollTop + 'px';
+		document.documentElement.style.position = 'fixed';
+		document.documentElement.style.overflowY = 'scroll';
+		document.body.style.overflowY = 'scroll';
+		document.documentElement.style.width = '100vw';
 	});
 
 	function clearValue(event: MouseEvent) {
 		if (event.target !== document.querySelector('#modal-backdrop-dataview')) return;
 		name.set(null);
+
+		// re-enable scrolling
+		const scrollTop = Math.abs(parseInt(document.documentElement.style.top, 10));
+		document.documentElement.removeAttribute('style');
+		document.body.removeAttribute('style');
+		document.documentElement.scrollTop = scrollTop;
+		document.body.scrollTop = scrollTop;
 	}
 </script>
 
@@ -39,7 +51,7 @@
 
 <style>
 	.backdrop {
-		position: absolute;
+		position: fixed;
 		top: 0;
 		left: 0;
 		right: 0;
@@ -50,8 +62,9 @@
 		justify-content: center;
 		align-items: center;
 		background-color: rgba(0, 0, 0, 0.2);
-		backdrop-filter: blur(6px);
+		backdrop-filter: blur(4px);
 		z-index: 99999;
+		overscroll-behavior: none;
 	}
 	.modal {
 		background-color: var(--tertiary-color);
@@ -59,5 +72,8 @@
 		width: 720px;
 		padding: 1rem;
 		border-radius: 4px;
+		max-height: 90vh;
+		overscroll-behavior: none;
+		scrollbar-gutter: stable;
 	}
 </style>
